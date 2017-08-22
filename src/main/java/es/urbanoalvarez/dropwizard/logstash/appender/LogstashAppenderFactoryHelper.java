092632ed78b71760cdcec262f40a45a01a8ffc76
Neftales Antunes <neftales@gmail.com>
2017-08-22 10:46:13 -0300

    Method for LogstashAccessFieldsNames.

--- src/main/java/es/urbanoalvarez/dropwizard/logstash/appender/LogstashAppenderFactoryHelper.java
+++ src/main/java/es/urbanoalvarez/dropwizard/logstash/appender/LogstashAppenderFactoryHelper.java
@@ -1,6 +1,7 @@
 package es.urbanoalvarez.dropwizard.logstash.appender;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import net.logstash.logback.fieldnames.LogstashAccessFieldNames;
 import net.logstash.logback.fieldnames.LogstashFieldNames;
 
 import java.io.IOException;
@@ -27,6 +28,28 @@ public class LogstashAppenderFactoryHelper {
     fieldNames.setTags(map.getOrDefault("tags", "tags"));
     fieldNames.setMdc(map.getOrDefault("mdc", null));
     fieldNames.setContext(map.getOrDefault("context", null));
+
+    return fieldNames;
+  }
+
+  public static LogstashAccessFieldNames getAccessFieldNamesFromHashMap(HashMap<String, String> map) {
+    LogstashAccessFieldNames fieldNames = new LogstashAccessFieldNames();
+
+    fieldNames.setTimestamp(map.getOrDefault("timestamp", "@timestamp"));
+    fieldNames.setVersion(map.getOrDefault("version", "@version"));
+    fieldNames.setMessage(map.getOrDefault("message", "message"));
+    fieldNames.setFieldsMethod(map.getOrDefault("fieldsMethod", "@fields.method"));
+    fieldNames.setFieldsProtocol(map.getOrDefault("fieldsProtocol", "@fields.protocol"));
+    fieldNames.setFieldsStatusCode(map.getOrDefault("fieldsStatusCode", "@fields.status_code"));
+    fieldNames.setFieldsRequestedUrl(map.getOrDefault("fieldsRequestedUrl", "@fields.requested_url"));
+    fieldNames.setFieldsRequestedUri(map.getOrDefault("fieldsRequestedUri", "@fields.requested_uri"));
+    fieldNames.setFieldsRemoteHost(map.getOrDefault("fieldsRemoteHost", "@fields.remote_host"));
+    fieldNames.setFieldsHostname(map.getOrDefault("fieldsHostname", "@fields.HOSTNAME"));
+    fieldNames.setFieldsRemoteUser(map.getOrDefault("fieldsRemoteUser", "@fields.remote_user"));
+    fieldNames.setFieldsContentLength(map.getOrDefault("fieldsContentLenght", "@fields.content_length"));
+    fieldNames.setFieldsElapsedTime(map.getOrDefault("fieldsElapsedTime", "@fields.elapsed_time"));
+    fieldNames.setFieldsRequestHeaders(map.getOrDefault("fieldsRequestHeaders", "@fields.request_headers"));
+    fieldNames.setFieldsResponseHeaders(map.getOrDefault("fieldsResponseHeaders", "@fields.response_headers"));
 
     return fieldNames;
   }
